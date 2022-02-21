d55c3afb082cc3adb2f644fbad1181919d77ecfe
Michael Finger <michael.finger@icytv.de>
2022-02-22 00:53:00 +0100

    Changed v8 checkout to run on configure

--- build.py
+++ build.py
@@ -38,8 +38,13 @@ parser.add_argument('gclient', action='store', type=pathlib.Path)
 parser.add_argument('ninja', action='store', type=pathlib.Path)
 parser.add_argument('--revision', action='store', type=str)
 parser.add_argument('--shared', action='store_false')
+parser.add_argument('--gen-headers', action='store_true')
+parser.add_argument('--build', action='store_true')
 
 args = parser.parse_args()
+
+assert(args.gen_headers or args.build,
+       'Must specify either --gen-headers or --build')
 
 start = time.time()
 print(bcolors.OKBLUE + "Setting up build directory %s %s..." %
@@ -54,112 +59,115 @@ os.chdir(build_dir)
 my_env = os.environ.copy()
 my_env['DEPOT_TOOLS_WIN_TOOLCHAIN'] = '0'
 
-subprocess.run(
-    [args.gclient, 'config', '--name', 'v8', '--unmanaged',
-        'https://chromium.googlesource.com/v8/v8.git'], shell=True, check=True,
-    env=my_env
-)
-
-print(bcolors.OKGREEN + "Syncing..." + bcolors.ENDC)
-
-subprocess.run(
-    [args.gclient, 'sync', '--revision', args.revision, '--shallow', '--no-history'], shell=True, check=True,
-    env=my_env
-)
-
-print(bcolors.OKGREEN + "Configuring with gn..." + bcolors.ENDC)
-
-os.chdir('v8')
-
 outname = 'out/x64.custom-%s' % args.target
 
-gn_args = [
-    'is_component_build=false',
-    'v8_monolithic=true',
-    'v8_static_library=%s' % ('true' if args.shared else 'false'),
-    'v8_use_external_startup_data=false',
-    'is_clang=%s' % ('true' if args.is_clang else 'false'),
-    'cc_wrapper="%s"' % args.cc_wrapper,
-    'is_debug=%s' % ('true' if args.target == 'debug' else 'false'),
-]
+if args.gen_headers:
+    if os.path.exists(os.path.join('v8', outname, 'args.gn')) and not args.build and os.path.exists(os.path.join(args.header_out, 'include')):
+        print("Reusing existing installation")
+        sys.exit(0)
 
-if args.shared:
-    gn_args.append('v8_expose_symbols=true')
+    subprocess.run(
+        [args.gclient, 'config', '--name', 'v8', '--unmanaged',
+         'https://chromium.googlesource.com/v8/v8.git'], shell=True, check=True,
+        env=my_env
+    )
 
-if args.target == 'debug':
-    gn_args.append('symbol_level=2')
-    gn_args.append('v8_symbol_level=2')
-    # gn_args.append('strip_absolute_paths_from_debug_symbols=true')
+    print(bcolors.OKGREEN + "Syncing..." + bcolors.ENDC)
 
-subprocess.run(
-    [args.gn, 'gen', outname, '--args=%s' % " ".join(gn_args)], shell=True, check=True,
-    env=my_env
-)
+    subprocess.run(
+        [args.gclient, 'sync', '--revision', args.revision, '--shallow', '--no-history', '-A'], shell=True, check=True,
+        env=my_env
+    )
 
-print(bcolors.OKGREEN + "Building v8 Monolith..." + bcolors.ENDC)
+    print(bcolors.OKGREEN + "Configuring with gn..." + bcolors.ENDC)
 
-subprocess.run(
-    [args.ninja, '-C', outname, 'v8_monolith'], shell=True, check=True,
-    env=my_env
-)
+    os.chdir('v8')
 
-delta = time.time() - start
+    gn_args = [
+        'is_component_build=false',
+        'v8_monolithic=true',
+        'v8_static_library=%s' % ('true' if args.shared else 'false'),
+        'v8_use_external_startup_data=false',
+        'is_clang=%s' % ('true' if args.is_clang else 'false'),
+        'cc_wrapper="%s"' % args.cc_wrapper,
+        'is_debug=%s' % ('true' if args.target == 'debug' else 'false'),
+    ]
 
-print(bcolors.OKGREEN + "Done. Took %.02fs" % delta + bcolors.ENDC)
+    if args.shared:
+        gn_args.append('v8_expose_symbols=true')
 
-# TODO - Add cross platform support. Currently only supports Windows.
-os.chdir('..')
+    if args.target == 'debug':
+        gn_args.append('symbol_level=2')
+        gn_args.append('v8_symbol_level=2')
+        # gn_args.append('strip_absolute_paths_from_debug_symbols=true')
 
-shutil.copy(
-    os.path.join('v8', outname, 'obj/v8_monolith.lib'),
-    'v8_monolith.lib'
-)
+    subprocess.run(
+        [args.gn, 'gen', outname, '--args=%s' % " ".join(gn_args)], shell=True, check=True,
+        env=my_env
+    )
 
-shutil.copy(
-    os.path.join('v8', outname, 'obj/v8_libplatform.lib'),
-    'v8_libplatform.lib'
-)
+    args.header_out.mkdir(parents=True, exist_ok=True)
 
-shutil.copy(
-    os.path.join('v8', outname, 'obj/v8_libbase.lib'),
-    'v8_libbase.lib'
-)
+    def keep(filename):
+        # TODO figure out a better way to do this
+        return filename.endswith('.h') or filename == 'libplatform'
 
-pdbs = [
-    'v8_base_without_compiler_0_cc.pdb',
-    'v8_cppgc_shared_cc.pdb',
-    'v8_initializers_cc.pdb',
-    'v8_init_cc.pdb',
-    'v8_libbase_cc.pdb',
-    'v8_libplatform_cc.pdb',
-    'v8_snapshot_cc.pdb',
-    'v8_compiler_cc.pdb',
-    'v8_bigint_cc.pdb',
-    'v8_base_without_compiler_1_cc.pdb',
-]
+    def ignore_non_headers(d, files):
+        return [f for f in files if not keep(f)]
 
-if sys.platform == 'win32':
-    for pdb in pdbs:
-        path = os.path.join('v8', outname, 'obj', pdb)
-        shutil.copy(path, pdb)
+    # TODO - Do we want to symlink?
+    shutil.copytree(
+        os.path.join(args.build_dir, 'v8', 'include/'),
+        os.path.join(args.header_out, 'include/'),
+        dirs_exist_ok=True,
+        ignore=ignore_non_headers
+    )
 
-if not args.header_out.exists():
-    args.header_out.mkdir()
+if args.build:
+    assert(os.path.exists(os.path.join('v8', outname)))
+    print(bcolors.OKGREEN + "Building v8 Monolith..." + bcolors.ENDC)
 
+    subprocess.run(
+        [args.ninja, '-C', os.path.join('v8', outname), 'v8_monolith'], shell=True, check=True,
+        env=my_env
+    )
 
-def keep(filename):
-    # TODO figure out a better way to do this
-    return filename.endswith('.h') or filename == 'libplatform'
+    delta = time.time() - start
 
+    print(bcolors.OKGREEN + "Done. Took %.02fs" % delta + bcolors.ENDC)
 
-def ignore_non_headers(d, files):
-    return [f for f in files if not keep(f)]
+    # TODO - Add cross platform support. Currently only supports Windows.
+    os.chdir('..')
 
+    shutil.copy(
+        os.path.join('v8', outname, 'obj/v8_monolith.lib'),
+        'v8_monolith.lib'
+    )
 
-# TODO - Do we want to symlink?
-shutil.copytree(
-    os.path.join('v8', 'include/'),
-    os.path.join(args.header_out, 'include/'),
-    dirs_exist_ok=True,
-    ignore=ignore_non_headers
-)
+    shutil.copy(
+        os.path.join('v8', outname, 'obj/v8_libplatform.lib'),
+        'v8_libplatform.lib'
+    )
+
+    shutil.copy(
+        os.path.join('v8', outname, 'obj/v8_libbase.lib'),
+        'v8_libbase.lib'
+    )
+
+    pdbs = [
+        'v8_base_without_compiler_0_cc.pdb',
+        'v8_cppgc_shared_cc.pdb',
+        'v8_initializers_cc.pdb',
+        'v8_init_cc.pdb',
+        'v8_libbase_cc.pdb',
+        'v8_libplatform_cc.pdb',
+        'v8_snapshot_cc.pdb',
+        'v8_compiler_cc.pdb',
+        'v8_bigint_cc.pdb',
+        'v8_base_without_compiler_1_cc.pdb',
+    ]
+
+    if sys.platform == 'win32':
+        for pdb in pdbs:
+            path = os.path.join('v8', outname, 'obj', pdb)
+            shutil.copy(path, pdb)
